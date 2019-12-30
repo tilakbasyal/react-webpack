814b7cc6f82d6f5c258d872bcc14a4fa7b5363ad
Tilak Basyal <basyaltilak143@gmail.com>
2019-12-30 11:11:46 +0545

    webpack basic configuration

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>This is a boiler plate. Include your componets here..</h1>
+        </div>
+    )
+}
+
+export default App
